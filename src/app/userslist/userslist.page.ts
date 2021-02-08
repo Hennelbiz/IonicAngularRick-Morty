8e3069e6c2cca831f11314ddb0277b2b7f01f816
Hennel Martinez Honorato <bizmarck.martinez@lamarina.mx>
2021-02-08 11:43:51 -0600

    Se comentaron lineas de codigo

--- src/app/userslist/userslist.page.ts
+++ src/app/userslist/userslist.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
+
 @Component({
   selector: 'app-userslist',
   templateUrl: './userslist.page.html',
@@ -16,7 +17,7 @@ export class UserslistPage implements OnInit {
     this.httpClient.get<any>('https://rickandmortyapi.com/api/character')
     .subscribe(res => 
     {
-      console.log(res);
+      //console.log(res);
       this.characters = res.results;
     })
   }
